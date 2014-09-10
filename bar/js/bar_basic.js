e0466591813bf95fb66d8575c8bfc68ba6d334e2
宁朗 <ninglang.wx@alibaba-inc.com>
2014-09-10 10:49:43 +0800

    add bar

--- bar/js/bar_basic.js
+++ bar/js/bar_basic.js
@@ -1,6 +1,6 @@
 ;(function (exports) {
-  var Line = exports.Line = function (container, options) {
-    this.container = container;
+  var Bar = exports.Bar = function (container, options) {
+  	this.container = container;
     var dom = $("#" + container);
 
     var defaults = {};
@@ -9,10 +9,15 @@
     defaults.margin = options.margin || {top: 20, right: 20, bottom: 30, left: 50};
     defaults.max = options.max || 100;
     defaults.min = options.min || 0;
-    defaults.gridNum = options.gridNum || 4;
-    defaults.xAxisStyle = options.xAxisStyle || {'fill': 'none', 'stroke': '#000', 'stroke-width': '1px'};
-    defaults.yAxisStyle = options.yAxisStyle || {'fill': 'none', 'stroke': '#000', 'stroke-width': '1px'};
-    defaults.lineStyle = options.lineStyle || {'fill': 'none', 'stroke-width': '1.5px'};
+    defaults.gridNum = options.gridNum || 3;
+    var valueStep = (defaults.max - defaults.min) / defaults.gridNum;
+    var min = defaults.min;
+    var max = defaults.max;
+    defaults.yTickValues = options.yTickValues || [min, min + valueStep, max - valueStep, max];
+    defaults.yValueFormat = options.yValueFormat || 'f';
+    defaults.xAxisStyle = options.xAxisStyle || {'fill': 'none', 'stroke': '#000', 'shape-rendering': 'crispEdges'};
+    defaults.yAxisStyle = options.yAxisStyle || {'fill': 'none', 'stroke': '#000', 'shape-rendering': 'crispEdges'};
+    defaults.barStyle = options.lineStyle || {'stroke': 'none'};
     defaults.colorList = options.colorList || ['#eee', '#d6e685', '#8cc665', '#44a340', '#1e6823'];
     defaults.getColor = options.getColor || function (num) {
       return defaults.colorList[num % defaults.colorList.length];
@@ -21,16 +26,17 @@
     this.defaults = defaults;
   };
 
-  Line.prototype.loadData = function (data) {
+  Bar.prototype.loadData = function (data) {
     if (!data) {
       return false;
     }
     this.data = data;
   };
 
-  Line.prototype._getSVG = function () {
+  Bar.prototype._getSVG = function () {
     var conf = this.defaults;
     var container = this.container;
+    $(container).empty();
 
     var svg = d3.select("#" + container).append("svg")
       .attr({
@@ -39,9 +45,9 @@
       });
 
     return svg;
-  }
+  };
 
-  Line.prototype.render = function (callback) {
+  Bar.prototype.render = function (callback) {
     var svg = this._getSVG();
     var conf = this.defaults;
     var margin = conf.margin;
@@ -52,66 +58,63 @@
     var gridNum = conf.gridNum;
     var getColor = conf.getColor;
 
-    var data = this.data;
-
     var paper = svg.append('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
     var x = d3.scale.ordinal()
-        .rangeRoundBands([0, width], .1);
+      .rangeBands([0, width], 0.5, 0.1);
 
     var y = d3.scale.linear()
-        .range([height, 0]);
+      .range([height, 0]);
 
-    x.domain(data.map(function(d) { return d.date; }));
+    x.domain(data.map(function (d) { return d.name; }));
     y.domain([min, max]);
+    console.log(data.map(function (d) { return d.name; }));
 
     var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient('bottom');
+	    .scale(x)
+	    .orient("bottom");
 
-    var yTicks = [];
+		var yTicks = [];
     var yStep = (max - min) / (gridNum - 1);
     for (var i = 0; i < gridNum; i++) {
       yTicks.push(min + yStep * i);
     }
 
     var yAxis = d3.svg.axis()
-        .scale(y)
-        .tickValues(yTicks)
-        .orient('left');
+      .scale(y)
+      .tickValues(conf.yTickValues)
+      .tickFormat(d3.format(conf.yValueFormat))
+      .orient('left');
 
     var xAxisNode = paper.append('g')
-        .attr({
-          'class': 'x axis',
-          'transform': 'translate(0,' + height + ')'
-        })
-        .call(xAxis);
+      .attr({
+        'class': 'x axis',
+        'transform': 'translate(0,' + height + ')'
+      })
+      .call(xAxis);
 
     xAxisNode.selectAll('.axis path, .axis line').style(conf.xAxisStyle);
 
     var yAxisNode = paper.append('g')
-        .attr({ 'class': 'y axis' })
-        .call(yAxis);
+      .attr({ 'class': 'y axis' })
+      .call(yAxis);
 
     yAxisNode.selectAll('.axis path, .axis line').style(conf.yAxisStyle);
 
-    var line = d3.svg.line()
-        .x(function(d) { return x(d.date) + x.rangeBand() / 2; })
-        .y(function(d) { return y(d.value); });
-
-    paper.append('path')
-      .datum(data)
+    var bars = paper.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
       .attr({
-        'class': 'line',
-        'd': line,
-        'stroke': getColor(0),
-      })
-      .style(conf.lineStyle);
+        "class": "bar",
+        "x": function (d) { return x(d.name); },
+        "y": function (d) { return y(d.value); },
+        "width": function (d) { return x.rangeBand(); },
+        "height": function (d) { return y(min) - y(d.value); },
+        "fill": function (d, num) { return getColor(num); }
+      });
 
     callback = callback || function () {;};
     callback();
   };
-
 })(window.Chart = window.Chart || {});
-
